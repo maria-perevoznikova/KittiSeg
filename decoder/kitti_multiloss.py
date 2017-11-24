091ef957a314a14ce3384dfc50a703f872c02030
Maria Schoenholzer <maria.perevoznikova@gmail.com>
2017-11-24 19:06:48 +0100

    Modify encoder and loss functions implementation to use with more than 2 segmentation classes.

--- decoder/kitti_multiloss.py
+++ decoder/kitti_multiloss.py
@@ -60,13 +60,13 @@ def loss(hypes, decoded_logits, labels):
     Returns:
       loss: Loss tensor of type float.
     """
+    num_classes = hypes['arch']['num_classes']
     logits = decoded_logits['logits']
     with tf.name_scope('loss'):
-        logits = tf.reshape(logits, (-1, 2))
-        shape = [logits.get_shape()[0], 2]
+        logits = tf.reshape(logits, (-1, num_classes))
         epsilon = tf.constant(value=hypes['solver']['epsilon'])
         # logits = logits + epsilon
-        labels = tf.to_float(tf.reshape(labels, (-1, 2)))
+        labels = tf.to_float(tf.reshape(labels, (-1, num_classes)))
 
         softmax = tf.nn.softmax(logits) + epsilon
 
@@ -106,7 +106,8 @@ def _compute_cross_entropy_mean(hypes, labels, softmax):
 
 
 def _compute_f1(hypes, labels, softmax, epsilon):
-    labels = tf.to_float(tf.reshape(labels, (-1, 2)))[:, 1]
+    num_classes = hypes['arch']['num_classes']
+    labels = tf.to_float(tf.reshape(labels, (-1, num_classes)))[:, 1]
     logits = softmax[:, 1]
     true_positive = tf.reduce_sum(labels*logits)
     false_positive = tf.reduce_sum((1-labels)*logits)
@@ -146,9 +147,10 @@ def evaluation(hyp, images, labels, decoded_logits, losses, global_step):
     # It returns a bool tensor with shape [batch_size] that is true for
     # the examples where the label's is was in the top k (here k=1)
     # of all logits for that example.
+    num_classes = hyp['arch']['num_classes']
     eval_list = []
-    logits = tf.reshape(decoded_logits['logits'], (-1, 2))
-    labels = tf.reshape(labels, (-1, 2))
+    logits = tf.reshape(decoded_logits['logits'], (-1, num_classes))
+    labels = tf.reshape(labels, (-1, num_classes))
 
     pred = tf.argmax(logits, dimension=1)
 
