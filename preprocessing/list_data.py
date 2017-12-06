126245c2a40cbb1450369de949d6f0b82c2c0c73
Maria Schoenholzer <maria.perevoznikova>
2017-12-06 11:55:04 +0100

    Add preprocessing scripts.

--- preprocessing/list_data.py
+++ preprocessing/list_data.py
@@ -1,15 +1,9 @@
 import os
-import logging
-import tensorflow as tf
-
-flags = tf.app.flags
-FLAGS = flags.FLAGS
 
 gt_prefix = 'gt_'
-
-flags.DEFINE_string('input_folder', None,
-                    'Folder with input images.')
-
+# input_folder = "/home/maria/Documents/MLdata/train"
+# input_folder = "/home/maria/Documents/MLdata/val"
+input_folder = "/home/maria/Documents/MLdata/test"
 
 def listTestData(data_dir, output):
     files = [os.path.join(data_dir, line) for line in os.listdir(data_dir)]
@@ -32,28 +26,21 @@ def listTrainData(input_folder, input_dirs, output):
 
 
 def main():
-    input_folder = FLAGS.input_folder
-
-    if input_folder is None:
-        logging.error("No input_folder was given.")
-        logging.info("Usage: python list_data.py --input_folder data/training")
-        exit(1)
-
     if not os.path.exists(input_folder):
-        logging.error("Input folder {} doesn't exist".format(FLAGS.input_folder))
+        print("Input folder {} doesn't exist".format(input_folder))
         exit(1)
 
     input_dirs = os.listdir(input_folder)
     input_size = len(input_dirs)
     if input_size == 0:
-        logging.error("Input folder {} is empty".format(FLAGS.input_folder))
+        print("Input folder {} is empty".format(input_folder))
         exit(1)
     elif input_size == 1:
         listTestData(os.path.join(input_folder, input_dirs[0]), os.path.join(input_folder, "test.txt"))
     elif input_size == 2:
         listTrainData(input_folder, input_dirs, os.path.join(input_folder, "train.txt"))
     else:
-        logging.error("Input folder {} contains too many files/folders".format(FLAGS.input_folder))
+        print("Input folder {} contains too many files/folders".format(input_folder))
         exit(1)
 
 
